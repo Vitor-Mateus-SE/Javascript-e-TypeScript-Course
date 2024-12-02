cc2659e868746ecc6bb9921f936de2aca070a9d1
unknown <vitor.paiva.pessoal@gmail.com>
2024-12-02 08:20:05 -0300

    vars with LET

---
+++ Class JS basic/Class_6/index.js
@@ -0,0 +1,14 @@
+let name = "João Luiz";
+let nameThree; //star a variable without value
+
+console.log(name, " was born in 1984. ");
+console.log("At 2000 ", name, " meet Maria.");
+console.log(name, " marry with Maria at 2012.");
+console.log("Maria has one sun with ", name, " in 2015");
+console.log("The ", name, " Sun´s is called Eduardo");
+
+console.log(nameThree);
+nameThree = "test"; //giving a value to variable
+console.log(nameThree);
+nameThree = "test2";
+console.log(nameThree);
