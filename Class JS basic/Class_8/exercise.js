91548583fb57490bf07b073f80f4bf05966cf2d0
unknown <vitor.paiva.pessoal@gmail.com>
2024-12-03 08:23:50 -0300

    const and exercise

---
+++ Class JS basic/Class_8/exercise.js
@@ -0,0 +1,21 @@
+/* write this sentence using let or const, Vitor Mateus has 23 years old, weighs 80 kg and has 1.79 High and
+your IMC is (calculate)*/
+const name = "Vitor Mateus";
+const years = 23;
+const heightInKg = 80;
+const high = 1.79;
+let imcCalculated = heightInKg / (high * high);
+let yearBorn = 2024 - years;
+console.log(
+  name,
+  "has",
+  years,
+  "old, wheighs",
+  heightInKg,
+  "kg and has",
+  high,
+  "High and your IMC is ",
+  imcCalculated,
+  "and was born in ",
+  yearBorn
+);
---
+++ Class JS basic/class_7/index.js
@@ -0,0 +1,7 @@
+//Const, same rules at let but can´t change the value later
+const name = "Vitor";
+console.log(name, "is he name");
+let name2 = name;
+console.log(name2)
+
+console.log(typeof name)
