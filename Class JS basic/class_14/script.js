c8c8cefd6812c6ace1181bbc5778e40ffc5d9b6d
Vitor Mateus <vitor.paiva.pessoal@gmail.com>
2025-02-05 20:40:06 -0300

    strings

---
+++ Class JS basic/class_14/script.js
@@ -0,0 +1,11 @@
+//Strings again
+let oneString = '"One Text"';
+console.log(oneString);
+let twoString = ' "two Text" ';
+console.log(oneString);
+console.log(oneString[2]);
+console.log(` ${oneString} in a code `);
+console.log(oneString.indexOf("Text"));
+console.log(oneString.toUpperCase);
+console.log(oneString.toLowerCase);
+console.log(oneString.slice);
