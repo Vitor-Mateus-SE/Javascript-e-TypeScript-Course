20565354a4cee21130f43fed166025d7c36fe115
Vitor Mateus <vitor.paiva.pessoal@gmail.com>
2025-02-14 08:18:51 -0300

    revision and exercise

---
+++ Class JS basic/class16/js/script.js
@@ -0,0 +1,8 @@
+let num1 = window.prompt("digite um número");
+let num2 = window.prompt("digite outro número");
+
+num1 = Number(num1);
+num2 = Number(num2);
+sum = num1 + num2;
+alert(`the sum of these numbers is ${sum}`);
+console.log(sum);
