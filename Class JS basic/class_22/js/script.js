5203b9ad210108c3da667075c0332e78cca791f0
Vitor Mateus <vitor.paiva.pessoal@gmail.com>
2025-04-10 07:56:40 -0300

    today learn and create model

---
+++ Class JS basic/class_22/js/script.js
@@ -0,0 +1,10 @@
+const time = 50;
+if (time >= 0 && time <= 11) {
+  console.log(`Good Morning`);
+} else if (time >= 12 && time <= 17) {
+  console.log("Good Afternoon");
+} else if (time >= 18 && time <= 23) {
+  console.log("Good evening ");
+} else {
+  console.log("have you sure this hour? but, hello is nice to see u :) ");
+}
